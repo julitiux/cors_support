514729ae9b7f589402619960a062a439252b13ec
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-01 23:03:47 -0600

    starting project cors support

---
+++ src/main/java/com/cors_support/cors_support/CorsSupportApplication.java
@@ -0,0 +1,13 @@
+package com.cors_support.cors_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cors_support/cors_support/CorsSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cors_support.cors_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
