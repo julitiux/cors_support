db99eac9174416079b0bcd52405d58b4db610f5d
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-01 23:41:48 -0600

    implement method getString into DemoController

--- src/main/java/com/cors_support/cors_support/webApi/DemoController.java
+++ src/main/java/com/cors_support/cors_support/webApi/DemoController.java
@@ -1,4 +1,16 @@
 package com.cors_support.cors_support.webApi;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+  @GetMapping(value = "/string")
+  public ResponseEntity<String> getString() {
+    return new ResponseEntity<>("This is a Demo", HttpStatus.OK);
+  }
+
 }
